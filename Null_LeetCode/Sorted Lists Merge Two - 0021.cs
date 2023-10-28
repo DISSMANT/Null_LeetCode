8da92f3be102fb156003140d0a9eec35727f895a
DISSMANT <dissmant@yandex.ru>
2023-10-28 06:20:56 +0500

    solvedAnotherTask

---
+++ Null_LeetCode/Sorted Lists Merge Two - 0021.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Sockets;
+
+namespace Null_LeetCode
+{
+    public class SortedListsMergeTwo0021
+    {
+        public ListNode MergeTwoLists(ListNode pointerLeft, ListNode pointerRight)
+        {
+            var final = new ListNode();
+            var tail = final;
+            
+            while (pointerLeft != null && pointerRight != null)
+            {
+                if (pointerLeft.val <= pointerRight.val)
+                {
+                    tail.next = pointerLeft;
+                    pointerLeft = pointerLeft.next;
+                }
+                else
+                {
+                    tail.next = pointerRight;
+                    pointerRight = pointerRight.next;
+                }
+
+                tail = tail.next;
+            }
+
+            if (pointerLeft != null)
+                tail.next = pointerLeft;
+            else if (pointerRight != null)
+                tail.next = pointerRight;
+
+            return final.next;
+        }
+    }
+}
