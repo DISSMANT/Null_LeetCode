b47b89cb09237d8e05862e4fc5e2e80f6764d92c
DISSMANT <dissmant@yandex.ru>
2023-10-29 01:52:21 +0500

    solverRemoveNthNodeFromEnd

---
+++ Null_LeetCode/Remove Nth Node From End of List - 0019.cs
@@ -0,0 +1,29 @@
+﻿namespace Null_LeetCode
+{
+    public class RemoveNthNodeFromEndOfList0019
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            if (head == null || head.next == null)
+                return null;
+            
+            var newHead = new ListNode { next = head };
+            head = newHead;
+            
+            var left = head;
+            var right = newHead;
+            for (var x = 0; x < n; x++)
+                right = right.next;
+            
+            while (right.next != null)
+            {
+                left = left.next;
+                right = right.next;
+            }
+            
+            left.next = left.next.next;
+            
+            return head.next;
+        }
+    }
+}
