8810cf77b0de9ede089d3a6112987ba24a2b61c9
DISSMANT <dissmant@yandex.ru>
2023-10-28 05:06:31 +0500

    SolverReversed

---
+++ Null_LeetCode/Reversed Linked List - 0206.cs
@@ -0,0 +1,34 @@
+﻿namespace Null_LeetCode
+{
+    public class ReversedLinkedList0206
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null)
+                return null;
+
+            var newHead = head;
+            if (head.next != null)
+            {
+                newHead = ReverseList(head.next);
+                head.next.next = head;
+            }
+
+            head.next = null;
+
+            return newHead;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
