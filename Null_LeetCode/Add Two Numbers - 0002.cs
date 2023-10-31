b42dd8d997c69a0191aaa290c8266619d59cd391
DISSMANT <dissmant@yandex.ru>
2023-10-31 10:01:45 +0500

    solved another problem

---
+++ Null_LeetCode/Add Two Numbers - 0002.cs
@@ -0,0 +1,32 @@
+﻿namespace Null_LeetCode
+{
+    public class AddTwoNumbers0002
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var buff = 0;
+            var newList = new ListNode();
+            var start = newList;
+
+            while (l1 != null || l2 != null || buff != 0)
+            {
+                var tempOne = l1?.val ?? 0;
+                var tempTwo = l2?.val ?? 0;
+                var tempSum = tempOne + tempTwo;
+                if (buff != 0)
+                    tempSum += buff;
+
+                buff /= 10;
+                buff += tempSum / 10;
+
+                newList.next = new ListNode(tempSum % 10);
+                newList = newList.next;
+
+                l1 = l1?.next;
+                l2 = l2?.next;
+            }
+            
+            return start.next;
+        }
+    }
+}
---
+++ Null_LeetCode/Copy List with Random Pointer - 0138.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+
+namespace Null_LeetCode
+{
+    public class CopyListWithRandomPointer0138
+    {
+        public Node CopyRandomList(Node head)
+        {
+            if (head == null) return null;
+            
+            var oldToNew = new Dictionary<Node, Node>();
+            
+            var tail = head;
+            
+            while (tail != null)
+            {
+                oldToNew.Add(tail, new Node(tail.val));
+                tail = tail.next;
+            }
+
+            tail = head;
+            while (tail != null)
+            {
+                oldToNew[tail].next = tail.next != null ? oldToNew[tail.next] : null;
+                oldToNew[tail].random = tail.random != null ? oldToNew[tail.random] : null;
+                tail = tail.next;
+            }
+
+            return oldToNew[head];
+        }
+    }
+
+    public class Node
+    {
+        public int val;
+        public Node next;
+        public Node random;
+
+        public Node(int _val)
+        {
+            val = _val;
+            next = null;
+            random = null;
+        }
+    }
+}
