07ef439f251684a3791c7c56076a1a727720d087
DISSMANT <dissmant@yandex.ru>
2023-11-04 13:24:22 +0500

    solvedLinkedListCycle

---
+++ Null_LeetCode/Linked List Cycle - 0141.cs
@@ -0,0 +1,20 @@
+﻿namespace Null_LeetCode
+{
+    public class LinkedListCycle0141
+    {
+        public bool HasCycle(ListNode head)
+        {
+            var slow = head;
+            var fast = head;
+
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (slow == fast) return true;
+            }
+
+            return false;
+        }
+    }
+}
