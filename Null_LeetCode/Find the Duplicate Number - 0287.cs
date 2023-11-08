3987e47749850e1507d4fbc314c26c888bd6df07
DISSMANT <dissmant@yandex.ru>
2023-11-08 16:29:56 +0500

    solveFindTheDuplicate

---
+++ Null_LeetCode/Find the Duplicate Number - 0287.cs
@@ -0,0 +1,27 @@
+﻿namespace Null_LeetCode
+{
+    public class FindTheDuplicateNumber0287
+    {
+        public int FindDuplicate(int[] nums)
+        {
+            var slow = 0;
+            var fast = 0;
+
+            while (slow != fast || fast + slow == 0)
+            {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
+
+            var findSlow = 0;
+
+            while (findSlow != slow)
+            {
+                findSlow = nums[findSlow];
+                slow = nums[slow];
+            }
+            
+            return slow;
+        }
+    }
+}
